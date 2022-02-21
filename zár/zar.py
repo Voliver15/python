b7ef3a08c479c772a791b405fb298f86b10c1845
Vucskics Olivér Imre <vucskicso20@ADY-NAGYATAD.SULINET.HU>
2022-02-21 12:34:11 +0100

    3. feladatig

--- zár/zar.py
+++ zár/zar.py
@@ -7,3 +7,37 @@ for egysor in f:
 f.close()
 
 print(kodok)
+
+#239451
+print("2. feladat")
+
+be=input("Adja meg, mi nyitja a zárat!: ")
+
+print("3. feladat")
+sorszam=1
+talalat=[]
+for kod in kodok:
+    if kod ==be:
+        talalat.append(sorszam)
+        
+    sorszam += 1
+
+#szám lista összefűzése
+print("A nyitó kódszámok sorai:" + " ".join(str(szam)for szam in talalat))
+
+
+
+#kicsit más megoldás
+talalat=[]
+for index,kod in enumerate(kodok,1):
+    if kod ==be:
+        talalat.append(index)
+        
+    sorszam += 1
+
+#szám lista összefűzése
+print("A nyitó kódszámok sorai:" + " ".join(str(szam)for szam in talalat))
+
+
+
+
