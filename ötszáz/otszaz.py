40e3b148a5c4149aabdede10fbeaba41d8f368e7
Vucskics Olivér Imre <vucskicso20@ADY-NAGYATAD.SULINET.HU>
2022-03-07 08:43:46 +0100

    5. a feladat

--- ötszáz/otszaz.py
+++ ötszáz/otszaz.py
@@ -12,3 +12,18 @@ print("A fizetések száma: " + str(kosar.count("F")))
 
 print("3. feladat")
 print("Az első vásárló " + str(kosar.index("F")) + " darab árucikket vásárolt.")
+
+print("4. feladat")
+sorszam=int(input("Vásárlás sorszáma: "))
+arunev=input("Árucikk neve: ")
+darab=int(input("Darabszám: "))
+
+
+aruindex=kosar.index(arunev)
+darablista=kosar[:aruindex]
+print(darablista)
+vasarlasdb=darablista.count("F") +1
+print(vasarlasdb)
+
+print("5. feladat")
+print("Az első vásárlás sorszáma: " + str(vasarlasdb))
