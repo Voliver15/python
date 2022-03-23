a8e4b578a5d67aae6c91860caebf3c3c254e03de
Vucskics Olivér Imre <vucskicso20@ADY-NAGYATAD.SULINET.HU>
2022-03-23 09:39:40 +0100

    5. feladatig

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,7 +1,55 @@
+
 f=open("titanic.txt")
 adatok=f.read().split("\n")
 f.close()
 
 print("2.feladat: "+ str(len(adatok)) +" darab")
 
+
 print("3.feladat: "+str(sum([int(e.split(";")[1])+ int(e.split(";")[2]) for e in adatok]))+ " fő")
+
+ossz=0
+for e in adatok:
+    temp=e.split(";")
+    tulelo=int(temp[1])
+    eltunt=int(temp[2])
+    ossz+=tulelo
+    ossz+=eltunt
+
+print("3.feladat: "+str(ossz)+" fő")
+
+kulcsszo=input("4.feladat: Kulcsszó: ")
+van=False
+
+#find()
+for e in adatok:
+    if e.find(kulcsszo)>=0:
+        van=True
+        break
+
+if van:
+    print("Van találat!")
+else:
+    print("Nincs találat!")
+
+
+adatok2=[]
+for e in adatok:
+    temp=e.split(";")
+    temp[1]=int(temp[1])
+    temp[2]=int(temp[2])
+    adatok2.append(temp)
+print(adatok2)
+
+talalat=[e for e in adatok if e.find(kulcsszo)>-1]
+#print(talalat)
+if len(talalat)>0:
+    print("Van találat!")
+else:
+    print("Nincs találat!")
+
+print("5.feladat")    
+
+
+
+
