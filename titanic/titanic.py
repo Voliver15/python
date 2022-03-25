0f13674b7f8c0b7786d420bd7f92b6946f6c2c99
Vucskics Olivér Imre <vucskicso20@ADY-NAGYATAD.SULINET.HU>
2022-03-25 09:39:40 +0100

    8.feladat

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -39,17 +39,31 @@ for e in adatok:
     temp[1]=int(temp[1])
     temp[2]=int(temp[2])
     adatok2.append(temp)
-print(adatok2)
+#print(adatok2)
 
-talalat=[e for e in adatok if e.find(kulcsszo)>-1]
+talalat=[e for e in adatok2 if e[0].find(kulcsszo)>-1]
 #print(talalat)
 if len(talalat)>0:
     print("Van találat!")
 else:
     print("Nincs találat!")
 
-print("5.feladat")    
+print("5.feladat: ")    
+for e in talalat:
+    print("\t"+e[0]+ " " + str((e[1]+e[2]))+ " fő")
+
+print("5.feladat: ")
+print("\r\n".join([ "\t"+e[0]+ " " + str((e[1]+e[2]))+ " fő" for e in talalat ]))
+
+print("6.feladat: ")
+arany=[]
+for e in adatok2:
+    if e[2]/(e[1]+e[2])>0.6:
+        arany.append(e[0])
+for e in arany:
+    print("\t"+e)
 
 
 
 
+        
