8f5e2c3267abcf8779a70246a7d2be98f118e6be
Vucskics Olivér Imre <vucskicso20@ADY-NAGYATAD.SULINET.HU>
2022-04-01 14:04:13 +0200

    nevberakas

--- hiányzások/hianyzasok.py
+++ hiányzások/hianyzasok.py
@@ -10,5 +10,11 @@ honap=0
 nap=0
 for e in adatok:
     if e[0]=="#":
-        
-
+        honap=e[2:4]
+        nap=e[5:]
+        #print(honap)
+    else:
+        temp=[]
+        temp.append(honap)
+        temp.append(nap)
+        #név + hiányzás berakása
