25a3611f9bcdc48d1e7446930f3f9ccd8814bcfb
Vucskics Olivér Imre <vucskicso20@ADY-NAGYATAD.SULINET.HU>
2022-04-11 12:35:55 +0200

    oopgyak

---
+++ oop1.py
@@ -0,0 +1,11 @@
+class Kutya:
+    def __init__(self,nev):
+        self.nev=nev
+
+    def ugat(self):
+        print("VAU-VAU ("+self.nev+")")
+        
+egyKutya=egyKutya("Armageddon")
+print(egyKutya.nev)
+egyKutya.ugat()
+    
