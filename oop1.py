48054efb24d5b3497ddbc6155e89d5cf3ba677a4
Your Name <you@example.com>
2022-04-12 08:30:34 +0200

    oop

--- oop1.py
+++ oop1.py
@@ -1,11 +1,34 @@
+#osztály definíció
 class Kutya:
+    #konstruktor
     def __init__(self,nev):
         self.nev=nev
-
+    #osztályfüggvény
     def ugat(self):
         print("VAU-VAU ("+self.nev+")")
         
-egyKutya=egyKutya("Armageddon")
-print(egyKutya.nev)
+#példányosítás
+        
+egyKutya=Kutya("Armageddon")
 egyKutya.ugat()
-    
+print(egyKutya.nev)
+#osztályváltozó értékadása
+
+egyKutya.nev="Bruno"
+print(egyKutya.nev)
+#öröklés,leszármaztatás
+
+class NemetJuhasz(Kutya):
+    #újfüggvény
+    def pitiz(self):
+        print(self.nev+": nein!")
+    #függvény felülírás
+    def ugat(self):
+        print("WAU-WAU")
+
+n=NemetJuhasz("Rex")
+n.ugat()
+n.pitiz()
+n2=NemetJuhasz("Kondér")
+n2.pitiz()
+n2.ugat()
