e653c9f0a357a5cf3420d163a266669f9b81e72a
Vucskics Olivér Imre <vucskicso20@ady-nagyatad.sulinet.hu>
2022-05-18 09:40:11 +0200

    sudoku 5.feladat

--- sodoku/sodoku.py
+++ sodoku/sodoku.py
@@ -1,11 +1,12 @@
+import math
 
 print("1. feladat")
 #filenev=input("Adja meg a bemeneti fájl nevét!: ")
 #sor=int(input("Adja meg egy sor számát!: "))
 #oszlop=int(input("Adja meg egy oszlop számát!: "))
 filenev="nehez.txt"
-sor=1
-oszlop=1
+sor=7
+oszlop=7
 
 
 f=open(filenev)
@@ -24,8 +25,8 @@ for _sor in adatok:
     
     adatok2.append(temp)
     
-print(adatok)
-print(adatok2)
+#print(adatok)
+#print(adatok2)
 
 tabla=adatok[:9]
 lepesek=adatok[9:]
@@ -33,7 +34,25 @@ lepesek=adatok[9:]
 #print(lepesek)
 
 print("3.feladat")
+if tabla[sor-1][oszlop-1]=="0":
+    print("Az adott helyet még nem töltötték ki.")
+else:
+    print("Az adott helyen szereplő szám: "+str(tabla[sor-1][oszlop-1]))
+    
+s=math.ceil(sor/3)-1
+o=math.ceil(oszlop/3)
 
-print("Az adott helyen szereplő szám: "+str(tabla[sor-1][oszlop-1]))
+
+print("A hely a(z) {} résztáblázathoz tartozik.".format(s*3+o))
+
+db=0
+for s in tabla:
+    db+=s.count("0")
+print("4.feladat")
+print("Az üres helyek aránya: {:.0%}".format(db/81))
+
+for lepes in lepesek:
+    
+
 
 
