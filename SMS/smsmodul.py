0250574d0309f99fb4cf75d1e968025aae02e7ae
Vucskics Olivér Imre <vucskicso20@ady-nagyatad.sulinet.hu>
2022-05-30 11:28:56 +0200

    tűzriadóesetén

--- SMS/smsmodul.py
+++ SMS/smsmodul.py
@@ -2,10 +2,10 @@ class adatok:
     def __init__(self,sor1,sor2):
         vag=sor1.split(" ")
         self.ora = int(vag[0])
-        slef.perc = int(vag[1])
-        slef.telefonszam = vag[2]
+        self.perc = int(vag[1])
+        self.telefonszam = vag[2]
 
-        slef.uzenet = sor2
+        self.uzenet = sor2
         
 
 
