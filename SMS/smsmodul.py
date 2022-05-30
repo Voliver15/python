2a5dd76ce6b56ae02c7353747841aa8f0d8eb823
Vucskics Olivér Imre <vucskicso20@ADY-NAGYATAD.SULINET.HU>
2022-05-30 08:44:47 +0200

    sms elsofeladat

---
+++ SMS/smsmodul.py
@@ -0,0 +1,17 @@
+class adatok:
+    def __init__(self,sor1,sor2):
+        vag=sor1.split(" ")
+        self.ora = int(vag[0])
+        slef.perc = int(vag[1])
+        slef.telefonszam = vag[2]
+
+        slef.uzenet = sor2
+        
+
+
+
+
+
+
+
+        
